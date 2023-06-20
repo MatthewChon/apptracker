c5594feee7c1581fa0d42bf84292962f31da5b63
Matthew Chon <matthewhchon@gmail.com>
2023-06-20 05:57:33 -0400

    created frontend

---
+++ static/scripts.js
@@ -0,0 +1,8 @@
+function drop_submission_form() {
+    const submission_form_elem = document.getElementById("submission-form");
+    submission_form_elem.classList.add("open");
+}
+function exit_submission_form() {
+    const submission_form_elem = document.getElementById("submission-form");
+    submission_form_elem.classList.remove("open");
+}
