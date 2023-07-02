44d38c94921f6fe008914daa5700579d2e00e91e
Matthew Chon <matthewhchon@gmail.com>
2023-07-02 01:03:03 -0400

    integrate data component in jinja

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ db.init_app(app)
 
 class Application(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    date_submitted = db.Column(db.DateTime)
+    date_submitted = db.Column(db.String(10))
     job_title = db.Column(db.String(50))
     company_name = db.Column(db.String(50))
     job_uri = db.Column(db.String(50))
@@ -24,6 +24,25 @@ class Application(db.Model):
 
 with app.app_context():
     db.create_all()
+def get_app_records():
+    app_records = {
+        "app_pending": get_data_by_type('pending'),
+        "app_missed": get_data_by_type('rejected'),
+        "app_hit": get_data_by_type('passed_screening')
+    }
+    app_records['overall_submission'] = app_records['app_hit'] \
+                                      + app_records['app_pending'] \
+                                      + app_records['app_missed']
+    return app_records
+
+def get_data_by_type(type='*'):
+    selected_query = db.session.query(Application).filter_by(status=type).all()
+    if selected_query is None:
+        return None
+    res = []
+    for row in selected_query:
+        res.append(row)
+    return res
 
 def is_valid_form_response(data, section):
     for section_title in section:
@@ -33,7 +52,16 @@ def is_valid_form_response(data, section):
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+    application_record = get_app_records()
+    pending_volume = len(application_record['app_missed'])
+    hit_volume = len(application_record['app_hit'])
+    missed_volume = len(application_record['app_missed'])
+    overall_volume = len(application_record['overall_submission'])
+    return render_template("index.html", application_record=application_record,
+                                         overall_volume=overall_volume,
+                                         pending_volume=pending_volume,
+                                         hit_volume=hit_volume,
+                                         missed_volume=missed_volume)
 
 @app.route("/post_application_data", methods=['POST'])
 def post_application_data():
@@ -41,7 +69,10 @@ def post_application_data():
     user_app = request.form
     if not is_valid_form_response(user_app, section):
         return redirect(url_for('index'))
-    user_submitted_data = Application(datetime.now(), user_app.get('job-title'), user_app.get('company-name'), user_app.get('job-uri'))
+    user_submitted_data = Application(datetime.now().strftime('%b %d %Y'),
+                                      user_app.get('job-title'),
+                                      user_app.get('company-name'),
+                                      user_app.get('job-uri'))
     db.session.add(user_submitted_data)
     db.session.commit()
     return redirect(url_for('index'))
